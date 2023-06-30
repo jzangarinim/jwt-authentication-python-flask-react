71572f701fd8a4513ae4efd905deb284ab9da7a8
Jose Zangarini <jzangarinim@gmail.com>
2023-06-30 16:14:44 -0400

    updated to bootstrap 5.3.0

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,15 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from werkzeug.security import generate_password_hash, check_password_hash
 
 api = Blueprint('api', __name__)
 
+def set_password(password, salt):
+    return generate_password_hash(f"{password}{salt}")
+
+def check_password(hashed_password, password, salt):
+    return check_password_hash(hashed_password, f"{password}{salt}")
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
